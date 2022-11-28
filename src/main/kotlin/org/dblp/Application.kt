fc62f056702d1f8a2f49822d7d5956bb66192f71
imsumoregn <87747324+imsumoregn@users.noreply.github.com>
2022-11-28 01:06:14 +0000

    perf: sweeping up a bit

--- src/main/kotlin/org/dblp/Application.kt
+++ src/main/kotlin/org/dblp/Application.kt
@@ -6,53 +6,14 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.doublereceive.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.dblp.db.initDbConnection
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.time.ZonedDateTime
 
-fun main(): Unit = runBlocking {
-    /**
-     * This cron job implementation follows the following link.
-     * @link: https://stackoverflow.com/questions/59516599/how-to-run-cron-jobs-in-kotlin-ktor
-     *
-     * TODO: Inspect performance difference between this implementation and using an actual library.
-     * @link: https://github.com/justwrote/kjob
-     * */
-
-    var hasBeenInitialised = false
-    val now = ZonedDateTime.now().plusSeconds(1)
-
-    launch {
-
-        if (!hasBeenInitialised) {
-
-            // No matter the time of installation, the first check will always be at 9:00 next day.
-            val tomorrowMorningAtNine = now
-                .plusDays(1)
-                .withHour(9)
-                .minusMinutes(now.minute.toLong())
-
-            delay(tomorrowMorningAtNine.toInstant().toEpochMilli() - now.toInstant().toEpochMilli())
-
-            checkRegisteredIssueStatus()
-
-            hasBeenInitialised = true
-
-        }
-
-        while (hasBeenInitialised) {
-
-            // Daily check at 9:00.
-            delay(24 * 60 * 60 * 1000)
-            checkRegisteredIssueStatus()
-
-        }
-
-    }
+@OptIn(DelicateCoroutinesApi::class)
+suspend fun main() {
 
     embeddedServer(Netty, port = 8080, watchPaths = listOf("classes")) {
 
@@ -66,7 +27,6 @@ fun main(): Unit = runBlocking {
             val user = System.getenv("myuser").toString()
             val password = System.getenv("mypassword").toString()
             val database = System.getenv("mydatabase").toString()
-            log.info("Connecting to database: $host, $user, $password, $database")
 
             try {
 
@@ -89,6 +49,40 @@ fun main(): Unit = runBlocking {
         configureRouting()
 
     }.start(wait = true)
+
+    /**
+     * This cron job implementation follows the following link.
+     * @link: https://stackoverflow.com/questions/59516599/how-to-run-cron-jobs-in-kotlin-ktor
+     *
+     * TODO: Inspect performance difference between this implementation and using an actual library.
+     * @link: https://github.com/justwrote/kjob
+     * */
+    GlobalScope.launch {
+
+        val now = ZonedDateTime.now().plusSeconds(1)
+
+        /** No matter the time of installation, the first check will always be at 9:00 next day. **/
+        val tomorrowMorningAtNine = now
+            .plusDays(1)
+            .withHour(9)
+            .minusMinutes(now.minute.toLong())
+
+        delay(tomorrowMorningAtNine.toInstant().toEpochMilli() - now.toInstant().toEpochMilli())
+
+        cronCheckRegisteredIssueStatus()
+
+        val hasBeenInitialised = true
+
+        while (hasBeenInitialised) {
+
+            /** Daily check at 9:00. **/
+            delay(24 * 60 * 60 * 1000)
+            cronCheckRegisteredIssueStatus()
+
+        }
+
+    }
+
 }
 
 val config: Config by lazy { ConfigFactory.load() }
