d365ebf159a9625b5d57e5fa95de385cb4162653
imsumoregn <87747324+imsumoregn@users.noreply.github.com>
2022-12-08 02:54:12 +0000

    ref(cron): cozy morning is the time when you reflect your *schewpid* looking code and fix it

--- src/main/kotlin/org/dblp/Application.kt
+++ src/main/kotlin/org/dblp/Application.kt
@@ -10,78 +10,52 @@ import kotlinx.coroutines.*
 import org.dblp.db.initDbConnection
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.time.ZonedDateTime
 
 @OptIn(DelicateCoroutinesApi::class)
-suspend fun main() {
+fun main() {
 
-    embeddedServer(Netty, port = 8080, watchPaths = listOf("classes")) {
-
-        install(DoubleReceive)
-
-        var retries = 5
-
-        while (retries >= 0) {
-
-            val host = System.getenv("myhost").toString()
-            val user = System.getenv("myuser").toString()
-            val password = System.getenv("mypassword").toString()
-            val database = System.getenv("mydatabase").toString()
-
-            try {
-
-                initDbConnection(host, user, password, database)
-                break
-
-            } catch (e: Exception) {
-
-                log.error("Failed to connect to database. Retrying in 5 seconds.")
-                runBlocking {
-                    delay(5000)
-                }
-
-                retries--
-
-            }
-
-        }
-
-        configureRouting()
-
-    }.start(wait = true)
-
-    /**
-     * This cron job implementation follows the following link.
-     * @link: https://stackoverflow.com/questions/59516599/how-to-run-cron-jobs-in-kotlin-ktor
-     *
-     * TODO: Inspect performance difference between this implementation and using an actual library.
-     * @link: https://github.com/justwrote/kjob
-     * */
     GlobalScope.launch {
 
-        val now = ZonedDateTime.now().plusSeconds(1)
+        cronDailyIssueStatusCheck()
 
-        /** No matter the time of installation, the first check will always be at 9:00 next day. **/
-        val tomorrowMorningAtNine = now
-            .plusDays(1)
-            .withHour(9)
-            .minusMinutes(now.minute.toLong())
+    }
 
-        delay(tomorrowMorningAtNine.toInstant().toEpochMilli() - now.toInstant().toEpochMilli())
+    embeddedServer(Netty, port = 8080, watchPaths = listOf("classes"), module = Application::module).start(wait = true)
 
-        cronCheckRegisteredIssueStatus()
+}
 
-        val hasBeenInitialised = true
+fun Application.module() {
 
-        while (hasBeenInitialised) {
+    install(DoubleReceive)
 
-            /** Daily check at 9:00. **/
-            delay(24 * 60 * 60 * 1000)
-            cronCheckRegisteredIssueStatus()
+    var retries = 5
+
+    while (retries >= 0) {
+
+        val host = System.getenv("myhost").toString()
+        val user = System.getenv("myuser").toString()
+        val password = System.getenv("mypassword").toString()
+        val database = System.getenv("mydatabase").toString()
+
+        try {
+
+            initDbConnection(host, user, password, database)
+            break
+
+        } catch (e: Exception) {
+
+            log.error("Failed to connect to database. Retrying in 5 seconds.")
+            runBlocking {
+                delay(5000)
+            }
+
+            retries--
 
         }
 
     }
+
+    configureRouting()
 
 }
 
--- src/main/kotlin/org/dblp/CronDailyIssueStatusCheck.kt
+++ src/main/kotlin/org/dblp/CronDailyIssueStatusCheck.kt
@@ -1,5 +1,6 @@
 package org.dblp
 
+import kotlinx.coroutines.delay
 import org.dblp.db.AppInstallation
 import org.dblp.db.IssueRegistry
 import org.jetbrains.exposed.sql.select
@@ -10,8 +11,45 @@ import space.jetbrains.api.runtime.types.ApiIcon
 import space.jetbrains.api.runtime.types.ChatMessage
 import space.jetbrains.api.runtime.types.MessageOutline
 import java.time.LocalDate
+import java.time.ZonedDateTime
 
-suspend fun cronCheckRegisteredIssueStatus() {
+/**
+ * Proceeds a daily check for registered issues at 9am.
+ *
+ * TODO: Inspect performance difference between this implementation and using an actual library.
+ * @link: https://stackoverflow.com/questions/59516599/how-to-run-cron-jobs-in-kotlin-ktor
+ * @link: https://github.com/justwrote/kjob
+ */
+suspend fun cronDailyIssueStatusCheck() {
+
+    val now = ZonedDateTime.now().plusSeconds(1)
+
+    /** No matter the time of installation, the first check will always be at 9:00 next day. **/
+    val tomorrowMorningAtNine = now
+        .plusDays(1)
+        .withHour(9)
+        .minusMinutes(now.minute.toLong())
+
+    delay(tomorrowMorningAtNine.toInstant().toEpochMilli() - now.toInstant().toEpochMilli())
+
+    checkRegisteredIssueStatus()
+
+    val hasBeenInitialised = true
+
+    while (hasBeenInitialised) {
+
+        /** Daily check at 9:00. **/
+        delay(24 * 60 * 60 * 1000)
+        checkRegisteredIssueStatus()
+
+    }
+
+}
+
+/**
+ * Checks the status of all registered issues and notifies issuers if their issues are overdue.
+ */
+suspend fun checkRegisteredIssueStatus() {
 
     val unresolvedIssues = transaction {
         IssueRegistry
