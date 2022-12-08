b887d3c491cbd03bc0493f71558fd46438887c16
imsumoregn <87747324+imsumoregn@users.noreply.github.com>
2022-12-08 02:29:43 +0000

    chore(help): make them look less default

--- src/main/kotlin/org/dblp/command/CommandHelp.kt
+++ src/main/kotlin/org/dblp/command/CommandHelp.kt
@@ -1,7 +1,9 @@
 package org.dblp.command
 
 import space.jetbrains.api.runtime.helpers.message
-import space.jetbrains.api.runtime.types.*
+import space.jetbrains.api.runtime.types.ChatMessage
+import space.jetbrains.api.runtime.types.MessageStyle
+import space.jetbrains.api.runtime.types.MessageTextSize
 import kotlin.reflect.KSuspendFunction1
 
 suspend fun runHelpCommand(sendMessage: KSuspendFunction1<ChatMessage, Unit>) {
@@ -14,25 +16,27 @@ fun helpMessage(): ChatMessage {
             text("**List of available commands**", MessageStyle.PRIMARY, size = MessageTextSize.LARGE)
             fields {
                 supportedCommands.forEach {
-                    field(it.name, it.info)
+                    field("*${it.name}*", it.info)
                 }
             }
-            text("E.g., send `watch https://<your-org>.jetbrains.space/p/<project_key>/issues/<issue_number> 7` to watch that issue for a week.", MessageStyle.PRIMARY, size = MessageTextSize.SMALL)
+            text(
+                "E.g., send `watch https://<your-org>.jetbrains.space/p/<project_key>/issues/<issue_number> 7` to watch that issue for a week.",
+                MessageStyle.PRIMARY,
+                size = MessageTextSize.SMALL
+            )
         }
     }
 }
 
 fun helpMessageError(): ChatMessage {
     return message {
-        MessageOutline(
-            icon = ApiIcon("checkbox-checked"),
-            text = "Sorry, I don't understand you."
-        )
         section {
-            text("Error", MessageStyle.ERROR)
+            text("**:exclamation: Unable to process command**", MessageStyle.ERROR, MessageTextSize.LARGE)
+            text("\n")
+            text("**List of available commands**", MessageStyle.PRIMARY)
             fields {
                 supportedCommands.forEach {
-                    field(it.name, it.info)
+                    field("*${it.name}*", it.info)
                 }
             }
         }
